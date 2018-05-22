61dd4ed626ea790f8dd4a3f29bb31b72bbc3dc86
Lucas Walter <wsacul@gmail.com>
2018-05-21 22:23:59 -0700

    BSD 3 license

--- src/transform_point_cloud.cpp
+++ src/transform_point_cloud.cpp
@@ -1,5 +1,14 @@
 /* Copyright 2018 Lucas Walter
-BSD 3 licensed
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #include <geometry_msgs/TransformStamped.h>
 #include <sensor_msgs/PointCloud2.h>
@@ -20,7 +29,7 @@ class TransformPointCloud
 
   void pointCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
   {
-    ROS_INFO_STREAM(msg->header.frame_id << " " << target_frame_);
+    // ROS_INFO_STREAM(msg->header.frame_id << " " << target_frame_);
     #if 0
     // convert to pcl_ros and print a few points
     for (size_t i = 0; i < 8 && i < msg->points.size(); ++i)
