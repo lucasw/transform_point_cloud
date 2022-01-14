51f5fb97de76e43300dbcc4ce60d5e5a0c3acd00
Lucas Walter <wsacul@gmail.com>
2022-01-14 06:56:16 -0800

    reformat python script to support transforming outside of the callback at a different rate

--- scripts/transform_point_cloud_py.py
+++ scripts/transform_point_cloud_py.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+# Lucas Walter
+# transform a pointcloud into a new frame
+
+import copy
+from threading import Lock
 
 import rospy
 import tf2_ros
@@ -12,25 +17,37 @@ from transform_point_cloud.cfg import LookupTransformConfig
 
 class TransformPointCloud:
     def __init__(self):
+        self.lock = Lock()
         self.config = None
         self.tf_buffer = tf2_ros.Buffer(cache_time=rospy.Duration(12))
         self.tl = tf2_ros.TransformListener(self.tf_buffer)
         self.pub = rospy.Publisher("point_cloud_transformed", PointCloud2, queue_size=2)
+        self.dr_server = Server(LookupTransformConfig, self.dr_callback)
+        self.cloud_in = None
         self.sub = rospy.Subscriber("point_cloud", PointCloud2,
                                     self.point_cloud_callback, queue_size=2)
-        self.dr_server = Server(LookupTransformConfig, self.dr_callback)
 
     def dr_callback(self, config, level):
-        self.config = config
-        return self.config
+        with self.lock:
+            self.config = copy.deepcopy(config)
+        return config
 
     def point_cloud_callback(self, msg):
-        lookup_time = msg.header.stamp + rospy.Duration(self.config.offset_lookup_time)
-        target_frame = msg.header.frame_id if self.config.target_frame == "" else self.config.target_frame
-        source_frame = msg.header.frame_id if self.config.source_frame == "" else self.config.source_frame
+        with self.lock:
+            config = copy.deepcopy(self.config)
+
+        self.publish_point_cloud(config, msg, msg.header.stamp)
+
+        with self.lock:
+            self.cloud_in = msg
+
+    def publish_point_cloud(self, config, cloud_in, stamp):
+        lookup_time = stamp + rospy.Duration(config.offset_lookup_time)
+        target_frame = cloud_in.header.frame_id if config.target_frame == "" else config.target_frame
+        source_frame = cloud_in.header.frame_id if config.source_frame == "" else config.source_frame
         try:
             trans = self.tf_buffer.lookup_transform(target_frame, source_frame, lookup_time,
-                                                    rospy.Duration(self.config.timeout))
+                                                    rospy.Duration(config.timeout))
         except tf2.LookupException as ex:
             rospy.logwarn(str(lookup_time.to_sec()))
             rospy.logwarn(ex)
@@ -39,7 +56,7 @@ class TransformPointCloud:
             rospy.logwarn(str(lookup_time.to_sec()))
             rospy.logwarn(ex)
             return
-        cloud_out = do_transform_cloud(msg, trans)
+        cloud_out = do_transform_cloud(cloud_in, trans)
         self.pub.publish(cloud_out)
 
 
