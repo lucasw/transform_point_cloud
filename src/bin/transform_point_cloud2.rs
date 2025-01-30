c049007e4e8c0037f29f0eedb6ccec65a3508029
Lucas Walter <wsacul@gmail.com>
2025-01-30 15:46:37 -0800

    updated to work with latest roslibrust (fork) and ros_pointcloud2, needs adjustments to work with high rate point clouds though

--- src/bin/transform_point_cloud2.rs
+++ src/bin/transform_point_cloud2.rs
@@ -1,211 +1,97 @@
-/// Take in a camera info and target frame and find the intersection of the boundary
-/// of the camera fov and the xy plane in the target frame, publish out as a marker and polygon
-/// based on camera_info_to_plane.py/.cpp and renamed to avoid rosrun confusion with the C++ node
+/*!
+Take in a camera info and target frame and find the intersection of the boundary
+of the camera fov and the xy plane in the target frame, publish out as a marker and polygon
+based on camera_info_to_plane.py/.cpp and renamed to avoid rosrun confusion with the C++ node
 
-use nalgebra::point;  // , Rotation, Rotation3};
-use roslibrust::ros1::{NodeHandle, Publisher};
+cargo run --release -- _target_frame:=base_link point_cloud_in:=/sensor/point_cloud
+*/
+
+use clap::command;
+use nalgebra::point; // , Rotation, Rotation3};
+use roslibrust::ros1::NodeHandle;
+use roslibrust_util::sensor_msgs;
 use std::collections::HashMap;
 use tf_roslibrust::{
-    TfError,
-    TfListener,
     // tf_util,
     transforms::isometry_from_transform,
+    LookupTransform,
+    TfError,
+    TfListener,
 };
 use tokio::time::Duration;
 
-// this is already done in ros_pointcloud2, but that was fine in the case of tf_roslibrust and
-// tf_demo- is there something in the structure of ros_pointcloud2 that breaks this?
-roslibrust_codegen_macro::find_and_generate_ros_messages!();
-
-// copy paste from ros_pointcloud2/rpcl2/src/ros.rs,
-// but couldn't get that to work
-impl From<sensor_msgs::PointCloud2> for ros_pointcloud2::PointCloud2Msg {
-    fn from(msg: sensor_msgs::PointCloud2) -> Self {
-        Self {
-            header: ros_pointcloud2::ros::HeaderMsg {
-                seq: msg.header.seq,
-                stamp: ros_pointcloud2::ros::TimeMsg {
-                    sec: msg.header.stamp.secs as i32,
-                    nanosec: msg.header.stamp.nsecs,
-                },
-                frame_id: msg.header.frame_id,
-            },
-            dimensions: ros_pointcloud2::CloudDimensions {
-                width: msg.width,
-                height: msg.height,
-            },
-            fields: msg
-                .fields
-                .into_iter()
-                .map(|field| ros_pointcloud2::ros::PointFieldMsg {
-                    name: field.name,
-                    offset: field.offset,
-                    datatype: field.datatype,
-                    count: field.count,
-                })
-                .collect(),
-            endian: if msg.is_bigendian {
-                ros_pointcloud2::Endian::Big
-            } else {
-                ros_pointcloud2::Endian::Little
-            },
-            point_step: msg.point_step,
-            row_step: msg.row_step,
-            data: msg.data,
-            dense: if msg.is_dense {
-                ros_pointcloud2::Denseness::Dense
-            } else {
-                ros_pointcloud2::Denseness::Sparse
-            },
-        }
-    }
-}
-
-impl From<ros_pointcloud2::PointCloud2Msg> for sensor_msgs::PointCloud2 {
-    fn from(msg: ros_pointcloud2::PointCloud2Msg) -> Self {
-        sensor_msgs::PointCloud2 {
-            header: std_msgs::Header {
-                seq: msg.header.seq,
-                stamp: roslibrust_codegen::Time {
-                    secs: msg.header.stamp.sec as u32,
-                    nsecs: msg.header.stamp.nanosec,
-                },
-                frame_id: msg.header.frame_id,
-            },
-            height: msg.dimensions.height,
-            width: msg.dimensions.width,
-            fields: msg
-                .fields
-                .into_iter()
-                .map(|field| sensor_msgs::PointField {
-                    name: field.name,
-                    offset: field.offset,
-                    datatype: field.datatype,
-                    count: field.count,
-                })
-                .collect(),
-            is_bigendian: if msg.endian == ros_pointcloud2::Endian::Big {
-                true
-            } else {
-                false
-            },
-            point_step: msg.point_step,
-            row_step: msg.row_step,
-            data: msg.data,
-            is_dense: if msg.dense == ros_pointcloud2::Denseness::Dense {
-                true
-            } else {
-                false
-            },
-        }
-    }
-}
-
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+    simple_logger::SimpleLogger::new()
+        .with_level(log::LevelFilter::Info)
+        .init()?;
 
-    // need to have leading slash on node name and topic to function properly
-    // so figure out namespace then prefix it to name and topics
+    // don't want params and remaps to be mutable after init is done
+    let (nh, _full_node_name, params, remaps) = {
+        let mut params = HashMap::<String, String>::new();
+        params.insert("_name".to_string(), "transform_point_cloud".to_string());
+        params.insert("target_frame".to_string(), "odom".to_string());
+        let mut remaps = HashMap::<String, String>::new();
+        remaps.insert("point_cloud_in".to_string(), "point_cloud_in".to_string());
+        remaps.insert("point_cloud_out".to_string(), "point_cloud_out".to_string());
 
-    // string parameters
-    let mut param_str = HashMap::<String, String>::new();
-    param_str.insert("target_frame".to_string(), "map".to_string());
-    // need extra leading _ for name and ns
-    param_str.insert("_name".to_string(), "transform_point_cloud2".to_string());
-    param_str.insert("_ns".to_string(), "".to_string());
+        let (_ns, full_node_name, remaining_args) =
+            roslibrust_util::get_params_remaps(&mut params, &mut remaps);
 
-    // TODO(lucasw) can an existing rust arg handling library handle the ':=' ros cli args?
-    let args = std::env::args();
-    let mut args2 = Vec::new();
-    for arg in args {
-        let key_val: Vec<&str> = arg.split(":=").collect();
-        if key_val.len() != 2 {
-            args2.push(arg);
-            continue;
-        }
+        // using clap only for version reporting currently
+        let _matches = command!().get_matches_from(remaining_args);
 
-        let (mut key, val) = (key_val[0].to_string(), key_val[1].to_string());
-        if !key.starts_with("_") {
-            println!("unused arg pair {key}:={val}- need to prefix name with underscore");
-            continue;
-        }
-        key.replace_range(0..1, "");
+        let ros_master_uri =
+            std::env::var("ROS_MASTER_URI").unwrap_or("http://localhost:11311".to_string());
+        let nh = NodeHandle::new(&ros_master_uri, &full_node_name).await?;
+        log::info!("connected to roscore at {ros_master_uri}");
 
-        if param_str.contains_key(&key) {
-            param_str.insert(key, val);
-        } else {
-            println!("unused '{key}' '{val}'");
-        }
-    }
-    println!("{args2:?}");
-    println!("{param_str:?}");
+        (nh, full_node_name, params, remaps)
+    };
 
-    let ns = param_str.remove("_ns").unwrap();
-    let target_frame = param_str.remove("target_frame").unwrap();
+    let target_frame = params.get("target_frame").unwrap();
 
-    let full_node_name = &format!(
-        "/{}/{}",
-        &ns,
-        &param_str["_name"],
-        ).replace("//", "/");
-    println!("{}", format!("full ns and node name: {full_node_name}"));
-
-    let nh = NodeHandle::new(&std::env::var("ROS_MASTER_URI")?, full_node_name)
+    let point_cloud_out_topic = remaps.get("point_cloud_out").unwrap();
+    log::info!("publishing on '{point_cloud_out_topic}'");
+    let transformed_point_cloud_pub = nh
+        .advertise::<sensor_msgs::PointCloud2>(point_cloud_out_topic, 3, false)
         .await?;
-
-    let transformed_point_cloud_pub: Publisher<sensor_msgs::PointCloud2> = nh.advertise(&format!("{}/point_cloud_transformed_rust", ns.as_str()), 3).await?;
 
     // TODO(lucasw) don't need channels yet but later switch away from tokio::select and use
     // channels between threads/tasks?
-    // let (pc_tc, pc_rx) = tokio::sync::oneshot::channel();
+    // let (pc_tx, pc_rx) = tokio::sync::oneshot::channel();
     let mut clouds_in = std::collections::VecDeque::new();
 
-    // TODO(lucasw) remember leading ns or it won't work
-    let mut point_cloud_sub = nh.subscribe::<sensor_msgs::PointCloud2>(&format!("{}/point_cloud", ns.as_str()), 10).await?;
+    let point_cloud_in_topic = remaps.get("point_cloud_in").unwrap();
+    log::info!("subscribing to '{point_cloud_in_topic}'");
+    let mut point_cloud_sub = nh
+        .subscribe::<sensor_msgs::PointCloud2>(point_cloud_in_topic, 10)
+        .await?;
 
-    let mut listener = TfListener::new(&nh).await;
+    let listener = TfListener::new(&nh).await;
 
     // TODO(lucasw) make this a queue
     let mut update_interval = tokio::time::interval(Duration::from_millis(50));
+    let mut count = 0;
 
     loop {
         tokio::select! {
             _ = tokio::signal::ctrl_c() => {
-                println!("ctrl-c exiting");
+                log::info!("ctrl-c exiting");
                 break;
             }
-            // TODO(lucasw) move this into listener
-            rv = listener._dynamic_subscriber.next() => {
-                print!(".");
-                match rv {
-                    Some(Ok(tfm)) => {
-                        listener.update_tf(tfm);  // .await;
-                    },
-                    Some(Err(error)) => {
-                        // probably can't keep up with tf publish rate
-                        println!("{error}");
-                    },
-                    None => (),
-                }
-            }
-            rv = listener._static_subscriber.next() => {
-                print!("+");
-                match rv {
-                    Some(Ok(tfm)) => {
-                        listener.update_tf_static(tfm);  // .await;
-                    },
-                    Some(Err(error)) => {
-                        panic!("{error}");
-                    },
-                    None => (),
-                }
-            }
+            // TODO(lucasw) not sure if this works properly inside a select
+            // Need a ReceivedQueue in roslibrust_util along the lines of one that
+            // only stores the latest
             rv = point_cloud_sub.next() => {
                 // print!("c");
                 // let t0 = tf_util::duration_now();
                 match rv {
                     Some(Ok(pc2_msg)) => {
-                        println!("{} bytes, {} x {}", pc2_msg.data.len(), pc2_msg.width, pc2_msg.height);
+                        count += 1;
+                        if count % 200 == 0 {
+                            log::info!("{} bytes, {} x {}", pc2_msg.data.len(), pc2_msg.width, pc2_msg.height);
+                        }
                         // TODO(lucasw) this doesn't work, maybe didn't implement the From
                         // properly- or the macro generated sensor_msgs::PointCloud2 inside
                         // ros_pointcloud2 not the same as the one generated here?  Or they can't
@@ -216,7 +102,7 @@ async fn main() -> Result<(), anyhow::Error> {
                         clouds_in.push_back(pc2_msg);
                     },
                     Some(Err(error)) => {
-                        println!("rx error: {error}");
+                        log::info!("rx error: {error}");
                     },
                     None => (),
                 }
@@ -231,15 +117,15 @@ async fn main() -> Result<(), anyhow::Error> {
                         // an error (because there was a different error and don't
                         // try transforming the same cloud later)
                         let res = listener.lookup_transform(
-                            &target_frame,
+                            target_frame,
                             &cloud_in.header.frame_id,
-                            None,
+                            Some(cloud_in.header.stamp.clone()),
                         );
                         match res {
                             Ok(tfs) => {
                                 let stamp = cloud_in.header.stamp.clone();
                                 let cloud_in: ros_pointcloud2::PointCloud2Msg = cloud_in.into();  // .try_into_iter().unwrap();
-                                println!("{} to {}", cloud_in.header.frame_id, target_frame);
+                                log::info!("{} to {}", cloud_in.header.frame_id, target_frame);
                                 let cloud_to_target = isometry_from_transform(&tfs.transform);
                                 // TODO(lucasw) once this is a function if this fails then
                                 // return the error
@@ -253,7 +139,7 @@ async fn main() -> Result<(), anyhow::Error> {
                                             pt_out.y as f32,
                                             pt_out.z as f32,
                                     ));
-                                    // println!("{pt:?}");
+                                    // log::info!("{pt:?}");
                                 }
                                 let pc_out = ros_pointcloud2::PointCloud2Msg::try_from_vec(points_out).unwrap();
                                 let mut pc_out_msg: sensor_msgs::PointCloud2 = pc_out.into();
@@ -261,25 +147,25 @@ async fn main() -> Result<(), anyhow::Error> {
                                 pc_out_msg.header.frame_id = target_frame.clone();
                                 transformed_point_cloud_pub.publish(&pc_out_msg).await?;
 
-                                // println!("{cloud_to_target:?}");
-                                println!("clouds left: {}", clouds_in.len());
+                                // log::info!("{cloud_to_target:?}");
+                                log::info!("clouds left: {}", clouds_in.len());
                             },
                             Err(err) => match err {
-                                TfError::AttemptedLookUpInFuture(_, _) => {
+                                TfError::AttemptedLookupInFuture(_, _, _) => {
                                     print!("-");
                                     // try to process again later
                                     clouds_in.push_front(cloud_in);
                                 },
                                 _ => {
                                     // give up on this point cloud with any other error
-                                    println!("lookup err, discarding point cloud {err:?}");
+                                    log::warn!("lookup err, discarding point cloud {err:?}");
                                 },
                             },
                         }
                     },
                 }  // handle input cloud
             },  // update
-        }  // tokio select loop
+        } // tokio select loop
     }
 
     Ok(())
